f8d5fb500c96571b79c9cb683bcd5a4ef2c7e2e0
mohammed zato <zatomohammed231@gmail.com>
2025-07-06 01:26:06 +0000

    added new section

---
+++ src/components/contactsDropdown.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Contacts() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <h2>+233 553654326 / +233 204132137</h2>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h2>harunamohammed231@gmail.com</h2>
+        </div>
+      </div>
+    </>
+  );
+}
